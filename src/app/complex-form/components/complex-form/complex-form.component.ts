d49525621e592617ecc70288e2f070e366d41c0d
Abdoulaye Aliou Sall <gallebaba71@gmail.com>
2024-11-23 16:40:25 +0000

    Les formulaires complexes

--- src/app/complex-form/components/complex-form/complex-form.component.ts
+++ src/app/complex-form/components/complex-form/complex-form.component.ts
@@ -2,6 +2,11 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatCard, MatCardActions, MatCardSubtitle, MatCardTitle} from "@angular/material/card";
 import {MatButton} from "@angular/material/button";
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInput} from "@angular/material/input";
+import {MatRadioModule} from '@angular/material/radio';
+import {map, Observable, startWith} from "rxjs";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-complex-form',
@@ -12,7 +17,11 @@ import {MatButton} from "@angular/material/button";
     MatCardTitle,
     MatCardSubtitle,
     MatCardActions,
-    MatButton
+    MatButton,
+    MatFormFieldModule,
+    MatInput,
+    MatRadioModule,
+    AsyncPipe,
   ],
   templateUrl: './complex-form.component.html',
   styleUrl: './complex-form.component.scss'
@@ -20,42 +29,50 @@ import {MatButton} from "@angular/material/button";
 export class ComplexFormComponent implements OnInit {
 
   mainForm!: FormGroup;
-  personnalInfoForm!: FormGroup;
+  personalInfoForm!: FormGroup;
   emailCtrl!: FormControl;
   confirmEmailCtrl!: FormControl;
   emailForm!: FormGroup;
-  contactPreferenceCrtl!: FormControl;
+  contactPreferenceCtrl!: FormControl;
   phoneCtrl!: FormControl;
   passwordCtrl!: FormControl;
   confirmPasswordCtrl!: FormControl;
   loginInfoForm!: FormGroup;
+
+  showEmailCtrl$!: Observable<boolean>;
+  showPhoneCtrl$!: Observable<boolean>;
 
 
   constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
-    this.initMainForm();
     this.initFormControls();
+    this.initMainForm();
+    this.initFormObservable();
   }
 
 
-  private initMainForm() {
+  private initMainForm(): void {
     this.mainForm = this.formBuilder.group({
-
-    })
+      personalInfo: this.personalInfoForm,
+      contactPreference: this.contactPreferenceCtrl,
+      email: this.emailForm,
+      phone: this.phoneCtrl,
+      loginInfo: this.loginInfoForm
+    });
   }
 
   onSubmitForm() {
-
+    console.log(this.mainForm.value);
   }
 
   private initFormControls() {
-    this.personnalInfoForm = this.formBuilder.group({
+    this.personalInfoForm = this.formBuilder.group({
       firstName: ["", Validators.required],
       lastName: ["", Validators.required],
     })
-    this.contactPreferenceCrtl = this.formBuilder.control('email');
+    this.contactPreferenceCtrl = this.formBuilder.control('email');
     this.phoneCtrl = this.formBuilder.control('');
     this.emailCtrl = this.formBuilder.control('');
     this.confirmEmailCtrl = this.formBuilder.control('');
@@ -70,5 +87,18 @@ export class ComplexFormComponent implements OnInit {
       password: this.passwordCtrl,
       confirmPassword: this.confirmPasswordCtrl,
     })
+  }
+
+  private initFormObservable() {
+    this.showEmailCtrl$ = this.contactPreferenceCtrl.valueChanges.pipe(
+      startWith(this.contactPreferenceCtrl.value),
+      map(preference => preference === "email")
+    )
+    console.log(this.showEmailCtrl$)
+    this.showPhoneCtrl$ = this.contactPreferenceCtrl.valueChanges.pipe(
+      startWith(this.contactPreferenceCtrl.value),
+      map(preference => preference === "phone")
+    )
+    console.log(this.showPhoneCtrl$)
   }
 }
